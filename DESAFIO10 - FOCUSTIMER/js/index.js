e913d451cc8922113ec8d32af09cff0eb745bd60
Francielli Rodrigues <contato.franciellirodrigues@outlook.com>
2022-06-09 20:34:17 -0300

    .

---
+++ DESAFIO10 - FOCUSTIMER/js/index.js
@@ -0,0 +1,81 @@
+// DOM = Document Object Model
+//Event-driven = fluxo de controle determinado por eventos ou alterações de estado
+//Programação imperativa = Dizer o passo a passo
+//Callback = chamar de volta, uma função passada a outra função como argumento, que é então invocado dentro da função externa para completar algum tipo de rotina ou ação.
+
+//Refatoração: Mudar um código para deixá-lo mais entendivel
+//deixar o código mais performático
+//Sem alterar suas funcionalidades
+
+
+import Controls from "./controls.js"
+import Timer from "./timer.js"
+import Sound from "./sounds.js"
+import {
+  buttonPause,
+  buttonPlay,
+  buttonSet,
+  buttonSoundOff,
+  buttonSoundOn,
+  buttonStop,
+  minutesDisplay,
+  secondsDisplay,
+} from "./elements.js"
+
+const controls = Controls({
+  buttonPause,
+  buttonPlay,
+  buttonSet,
+  buttonStop
+})
+
+const timer = Timer({
+  minutesDisplay, 
+  secondsDisplay, 
+  resetControls: controls.reset
+})
+
+const sound = Sound()
+
+
+buttonPlay.addEventListener('click', function() {
+  controls.play()
+  timer.countdown()
+  sound.pressButton()
+})
+
+buttonPause.addEventListener('click', function() {
+  controls.pause()
+  timer.hold()
+  sound.pressButton()
+})
+
+buttonStop.addEventListener('click', function() {
+  controls.reset()
+  timer.reset()
+  sound.pressButton()
+})
+
+buttonSoundOff.addEventListener('click', function() {
+  buttonSoundOn.classList.remove('hide')
+  buttonSoundOff.classList.add('hide')
+  sound.bgAudio.pause()
+})
+
+buttonSoundOn.addEventListener('click', function() {
+  buttonSoundOn.classList.add('hide')
+  buttonSoundOff.classList.remove('hide')
+  sound.bgAudio.play()
+})
+
+buttonSet.addEventListener('click', function() {
+  let newMinutes = controls.getMinutes()
+
+  if (!newMinutes) {
+    timer.reset()
+    return
+  }
+
+  timer.updateDisplay(newMinutes, 0)
+  timer.updateMinutes(newMinutes)
+})
